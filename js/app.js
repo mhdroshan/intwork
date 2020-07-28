a147ca6627442702491597b80dc84b2b252166da
mhdroshan <mhdroshan@users.noreply.github.com>
2020-07-29 01:03:13 +0530

    kjasd

--- js/app.js
+++ js/app.js
@@ -3,14 +3,8 @@ const signInButton = document.getElementById('signin');
 const form1 = document.getElementById('logforms');
 const form2 = document.getElementById('sigforms');
 
-
-
-signUpButton.addEventListener('click', () => {
-    form1.classList.add("rightactive");
-    form2.classList.add("leftactive");
-    form1.classList.remove("rightactivebak");
-    form2.classList.remove("leftactivebak");
-});
+const cr = document.getElementById('cr');
+const si = document.getElementById('si');
 
 signInButton.addEventListener('click', () => {
     form1.classList.add("rightactivebak");
@@ -18,4 +12,24 @@ signInButton.addEventListener('click', () => {
     form1.classList.remove("rightactive");
     form2.classList.remove("leftactive");
     
+    cr.classList.remove("opno");
+    cr.classList.add("opyes");
+    si.classList.remove("opyes");
+    si.classList.add("opno");
+    
 });
+
+signUpButton.addEventListener('click', () => {
+    form1.classList.add("rightactive");
+    form2.classList.add("leftactive");
+
+
+    form1.classList.remove("rightactivebak");
+    form2.classList.remove("leftactivebak");
+
+    cr.classList.remove("opyes");
+    cr.classList.add("opno");
+    si.classList.remove("opno");
+    si.classList.add("opyes");
+});
+
