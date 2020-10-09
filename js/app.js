3708693047c649f76f87483c36e3ad0e0c531b6b
mhdroshan <mhdroshan@users.noreply.github.com>
2020-10-09 23:36:30 +0530

    jj

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const signUpButton = document.getElementById('signup');
-const signInButton = document.getElementById('signin');
+const signInButton = document.getElementById('signinlink');
 const form1 = document.getElementById('logforms');
 const form2 = document.getElementById('sigforms');
 
@@ -12,10 +12,10 @@ signInButton.addEventListener('click', () => {
     form1.classList.remove("rightactive");
     form2.classList.remove("leftactive");
     
-    cr.classList.remove("opno");
-    cr.classList.add("opyes");
-    si.classList.remove("opyes");
-    si.classList.add("opno");
+    cr.classList.remove("closed");
+    cr.classList.add("opened");
+    si.classList.remove("opened");
+    si.classList.add("closed");
     
 });
 
@@ -27,9 +27,37 @@ signUpButton.addEventListener('click', () => {
     form1.classList.remove("rightactivebak");
     form2.classList.remove("leftactivebak");
 
-    cr.classList.remove("opyes");
-    cr.classList.add("opno");
-    si.classList.remove("opno");
-    si.classList.add("opyes");
+    cr.classList.remove("opened");
+    cr.classList.add("closed");
+    si.classList.remove("closed");
+    si.classList.add("opened");
 });
 
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+
+
+
+  autoplay: true,
+  loop: true,
+  margin: 10,
+  width: 100,
+  touchDrag: true,
+
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+});
+
+var input = document.querySelector("#phone");
+window.intlTelInput(input, {
+  utilsScript: "js/utils.js",
+});
